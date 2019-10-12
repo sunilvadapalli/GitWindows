0a97cb84a61137e1236dfbf15b683b11f58093d6
Sunil Vadapalli <simplesunil@yahoo.com>
2019-10-12 21:55:56 +0530

    Third function addition

--- WebdriverTest/src/test/java/example/NewTest.java
+++ WebdriverTest/src/test/java/example/NewTest.java
@@ -20,6 +20,12 @@ public class NewTest {
 			String title = driver.getTitle();				 
 			Assert.assertTrue(title.contains("Demo Guru99 Page")); 		
 		}
+		@Test				
+		public void testEasy2() {	
+			driver.get("http://demo.guru99.com/test/guru99home/");  
+			String title = driver.getTitle();				 
+			Assert.assertTrue(title.contains("Demo")); 		
+		}
 		@BeforeTest
 		public void beforeTest() {	
 			System.setProperty("webdriver.chrome.driver", "C:\\Users\\sunil\\Downloads\\chromedriver_win32\\chromedriver.exe");
